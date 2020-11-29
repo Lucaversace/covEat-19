9fa18931b12598a061f092119169f646e44eeaa8
Victor <victor.tollemer@ynov.com*>
2020-11-29 16:56:38 +0100

    Front : add_dish , edit_dish, dish_list, home

--- src/main/java/fr/coveat/app/controller/IndexController.java
+++ src/main/java/fr/coveat/app/controller/IndexController.java
@@ -7,12 +7,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = {"/","add_dish"}, method = RequestMethod.GET )
-    public String add_dish() {
-
-        //model.addAttribute("message",message);
+    @RequestMapping(value = {"/","login"}, method = RequestMethod.GET )
+    public String login() {
 
         return "login";
+    }
+    
+    @RequestMapping(value = {"add_dish"}, method = RequestMethod.GET )
+    public String add_dish() {
 
+        return "add_dish";
+    }
+    
+    @RequestMapping(value = {"dish_list"}, method = RequestMethod.GET )
+    public String dish_list() {
+
+        return "dish_list";
+    }
+    
+    @RequestMapping(value = {"edit_dish"}, method = RequestMethod.GET )
+    public String edit_dish() {
+
+        return "edit_dish";
+    }
+    
+    @RequestMapping(value = {"home"}, method = RequestMethod.GET )
+    public String home() {
+
+        return "home";
     }
 }
--- src/main/java/fr/coveat/app/repository/HibernateForwardEngineering.java
+++ src/main/java/fr/coveat/app/repository/HibernateForwardEngineering.java
@@ -11,23 +11,23 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class HibernateForwardEngineering {
 
     public static void main(String[] args) {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
-
-        SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        Dish dish = new Dish();
-        dish.setName("Produit magueule");
-//        dish.setDesc("Produit magueule");
-//        dish.setImage_url("Produit magueule");
-//        dish.setPrice(50);
-
-        session.save(dish);
-        transaction.commit();
-
-        session.close();
-        factory.close();
+//        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+//
+//        SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+//        Session session = factory.openSession();
+//        Transaction transaction = session.beginTransaction();
+//
+//        Dish dish = new Dish();
+//        dish.setName("Produit magueule");
+////        dish.setDesc("Produit magueule");
+////        dish.setImage_url("Produit magueule");
+////        dish.setPrice(50);
+//
+//        session.save(dish);
+//        transaction.commit();
+//
+//        session.close();
+//        factory.close();
 
 
     }
