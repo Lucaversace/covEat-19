9df9e716694c8a99acd9f2cb116a6a44bef1461a
Mostacci <gregory.mostacci@ynov.com>
2020-12-05 16:54:56 +0100

    Add dish repo

--- src/main/java/fr/coveat/app/controller/IndexController.java
+++ src/main/java/fr/coveat/app/controller/IndexController.java
@@ -1,4 +1,10 @@
 package fr.coveat.app.controller;
 
+import fr.coveat.app.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class IndexController {
+
+    private DishRepository userRepository;
 }
---
+++ src/main/java/fr/coveat/app/repository/DishRepository.java
@@ -0,0 +1,4 @@
+package fr.coveat.app.repository;
+
+public class DishRepository {
+}
