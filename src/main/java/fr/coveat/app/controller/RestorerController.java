e741aef4d95eba4febce85faffc8887b29c6dd0f
RemiAloin <remi.aloin@ynov.com>
2020-12-11 13:08:00 +0100

    Routes for dishes and change css

--- src/main/java/fr/coveat/app/controller/RestorerController.java
+++ src/main/java/fr/coveat/app/controller/RestorerController.java
@@ -1,5 +1,6 @@
 package fr.coveat.app.controller;
 
+import fr.coveat.app.form.DishForm;
 import fr.coveat.app.model.Dish;
 import fr.coveat.app.model.Restaurant;
 import fr.coveat.app.repository.DishRepository;
@@ -13,37 +14,58 @@ public class RestorerController {
     @Autowired
     private DishRepository dishRepository;
 
-    @RequestMapping(value = {"/restorer/","/restorer/dishes"}, method = RequestMethod.GET )
+    @RequestMapping(value = {"/restorer/","/restorer"}, method = RequestMethod.GET )
     public String getDish(Model model) {
         model.addAttribute("dishes", dishRepository.findAll());
-        return "dish_list";
+        return "restorer/dish_list";
     }
 
     @RequestMapping(value = {"/restorer/add_dish"}, method = RequestMethod.GET )
     public String createDish(Model model) {
 
         model.addAttribute("dishForm", new Dish());
-        return "add_dish";
+        return "restorer/add_dish";
     }
 
     @RequestMapping(value = {"/restorer/add_dish"}, method = RequestMethod.POST )
     public String postCreateDish(@ModelAttribute("dish") Dish dish) {
-    	// A MODIFIER APRES LES SESSIONS
+    	// TODO : A MODIFIER APRES LES SESSIONS
+
     	Restaurant restaurant=new Restaurant(); 
     	restaurant.setId(1);
+
     	dish.setRestaurant(restaurant);
         dishRepository.save(dish);
 
         return "redirect:/restorer/";
     }
 
-    @RequestMapping(value = {"/restorer/edit_dish"}, method = RequestMethod.GET )
-    public String editDish() {
+    @RequestMapping(value = {"/restorer/{id}/edit_dish"}, method = RequestMethod.GET )
+    public String editDish(@PathVariable("id") Long id, Model model) {
 
-        return "edit_dish";
+        model.addAttribute("dishForm", dishRepository.findById(id));
+        return "restorer/edit_dish";
     }
 
-    @RequestMapping(value = {"/restorer/delete_dish"}, method = RequestMethod.GET )
+    @RequestMapping(value = {"/restorer/{id}/edit_dish"}, method = RequestMethod.POST )
+    public String editDish(@PathVariable("id") Long id, Model model, @ModelAttribute("dishForm") DishForm dishForm) {
+        Dish dish = dishRepository.getOne(id);
+
+        dish.setName(dishForm.getName());
+        dish.setPrice(dishForm.getPrice());
+        dish.setDescription(dishForm.getDescription());
+        dish.setImageUrl(dishForm.getImageUrl());
+//        String name = dishForm.getName();
+//        Double price = dishForm.getPrice();
+//        String description = dishForm.getDescription();
+//        String imageUrl = dishForm.getImageUrl();
+
+        dishRepository.save(dish);
+
+        return "redirect:/restorer/";
+    }
+
+    @RequestMapping(value = {"/restorer/{id}/delete_dish"}, method = RequestMethod.GET )
     public String deleteDish() {
 
         return "redirect:/restorer/";
---
+++ src/main/java/fr/coveat/app/form/DishForm.java
@@ -0,0 +1,13 @@
+package fr.coveat.app.form;
+
+import lombok.Data;
+
+@Data
+public class DishForm {
+
+    private String name;
+    private Double price;
+    private String description;
+    private String imageUrl;
+
+}
