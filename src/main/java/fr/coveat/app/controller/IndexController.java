26ff05cc7b0342820ed43ab9e761e0af6ddd2404
RemiAloin <remi.aloin@ynov.com>
2020-12-17 00:54:17 +0100

    Add dish page

--- src/main/java/fr/coveat/app/controller/IndexController.java
+++ src/main/java/fr/coveat/app/controller/IndexController.java
@@ -3,6 +3,7 @@ package fr.coveat.app.controller;
 import fr.coveat.app.repository.DishRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,5 +20,14 @@ public class IndexController {
     public String home(Model model) {
         model.addAttribute("dishes", dishRepository.findAll());
         return "home";
+    }
+
+    @RequestMapping(value = {"dish/{id}"}, method = RequestMethod.GET )
+    public String getDish(@PathVariable("id") Long id, Model model) {
+        if (dishRepository.existsById(id)) {
+            model.addAttribute("dish", dishRepository.getOne(id));
+            return "dish";
+        }
+        return "redirect:/";
     }
 }
